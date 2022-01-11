31d6516adf96915b517cee2def9a5e952ef44ac6
BezTTTT <Best_1614@hotmail.com>
2022-01-11 20:19:23 +0700

    lab15_2 completed

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-const int N = 5;
+const int N = 7;
 
 void inputMatrix(double [][N]);
 
@@ -18,4 +18,38 @@ int main(){
 	cout << "Result is \n";
 	showMatrix(B);
 	return 0;
+}
+void inputMatrix(double d[][N])
+{
+	for(int x=0;x<N;x++){
+		cout << "Row " << x+1 << ": " ;
+		for(int y=0;y<N;y++){
+			cin >> d[x][y];
+		}
+	}
+}
+
+void showMatrix(const bool d[][N])
+{
+	for (int i=0;i<N;i++){
+		for(int j=0 ; j<N; j++){
+		cout << d[i][j];
+		cout << " " ;
+		}
+		cout << endl;
+	}
+}
+void findLocalMax(const double d[][N], bool b[][N])
+{
+	for(int i =0;i<N;i++){
+		for(int x=0;x<N;x++){
+			if(i==N-1 || x==N-1 || i==0 || x==0 ){
+				b[i][x] = 0;
+			}
+			else if(d[i][x]>=d[i-1][x] && d[i][x]>=d[i+1][x] && d[i][x]>=d[i][x-1] && d[i][x]>=d[i][x+1]){
+				 b[i][x] = 1;
+			}
+			else b[i][x] = 0;
+		}
+	}
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+for (i = 0; i < N; i++) 
+        cout << d[i] << " "; 
+    	cout << endl;
